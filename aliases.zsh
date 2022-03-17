8e857856c2aa169c60b96bce2c7f5d453ff434ac
Daniel Thoma <dt@clickbar.dev>
2022-03-17 19:17:23 +0100

    update dotfiles

--- aliases.zsh
+++ aliases.zsh
@@ -26,9 +26,6 @@ alias composer="php -d memory_limit=-1 /opt/homebrew/bin/composer"
 alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
 alias watch="npm run watch"
 
-# Docker
-alias docker-composer="docker-compose"
-
 # Git
 alias gst="git status"
 alias gb="git branch"
--- fresh.sh
+++ fresh.sh
@@ -5,6 +5,7 @@ echo "Setting up your Mac..."
 # Check for Oh My Zsh and install if we don't have it
 if test ! $(which omz); then
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+  # TODO: shell gets reloaded and interrupts the installation process.
 fi
 
 # Check for Homebrew and install if we don't have it
@@ -13,6 +14,7 @@ if test ! $(which brew); then
 
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
+  # TODO: reload shell, so it can find homebrew.
 fi
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
